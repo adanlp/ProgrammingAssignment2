a9233feb7b387d55ce97c16921547bede64835cd
Adanlp <adanlp@live.com.mx>
2017-12-04 13:07:09 -0600

    Week 3 Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a matrix 
+##
+## In order to avoid the costly computation of the inverse of a
+## matrix, specially when done iteratevely, these functions
+## make use of scoping rules to cache the inverse of a matrix 
+## previosly computed, returning directly the data without 
+## recomputing but indicating it with a message. 
+##
+## Adanlp
+## 03/DEC/2017
 
-## Write a short comment describing this function
 
+
+## This funtion creates the special "matrix"
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL
+    set <- function(y){
+        x <<- y
+        i <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) i <<- solve
+    getinv <- function() i
+    list(set=set, get=get, setinv=setinv,getinv=getinv)
 }
 
-
-## Write a short comment describing this function
-
+## This funtion resolve if the inverse of the matrix has been previously
+## computed, then retuning the value directly; otherwise, compute the
+## inverse using the functions of the special "matrix"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)){
+        message("Getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data,...)
+    x$setinv(i)
+    i
 }
